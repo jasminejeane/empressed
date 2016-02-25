61f6c47faef8cf4fd3faf5cea794d81f10d58ab6
Jasmine Martin <jasminejeane@gmail.com>
2016-02-25 14:30:04 -0800

    Seeded 10 stylist and added flex wrap so info could wrap properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,305 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Stylist.create({first_name: "V", last_name: "Van", 
-	school: "General Assembly", salon: "We Heart Hair",
-	email: "me1@me.com", headshot: "bday.jpg", password: "password"})
+ Stylist.create({first_name: "Jamie" , last_name: "Smith" , speciality: "Cutting" , salon: "CODE Salon" ,
+       salon_address: "370 4th St San Francisco, CA 94107", headshot: "stylist1.jpg" , 
+       email: "mee1@me.com", phone: "555-555-5555", school: "Paul Mitchell" , cut: "true", color: "false" ,
+      updo: "true", natural: "false" , bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
 
 
-Stylist.create({first_name: "Vi", last_name: "Van", 
-	school: "General Assembly", salon: "We Heart Hair",
-	email: "me2@me.com", headshot: "bday.jpg", password: "password"})
+  Stylist.create({first_name: "Annabelle", last_name: "Jones" , speciality: "Color" , salon: "Rubio & Raven",
+       salon_address: "142 Fillmore St San Francisco, CA 94117", 
+       headshot: "stylist2.jpg" , email: "mee2@me.com",
+       phone: "555-555-5555", school: "Aveda Institute", cut: "true", color: "true",
+      updo: "false" , natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
 
 
-Stylist.create({first_name: "Vie", last_name: "Van", 
-	school: "General Assembly", salon: "We Heart Hair",
-	email: "me3@me.com", headshot: "bday.jpg", password: "password"})
+   Stylist.create({first_name: "Peter" , last_name: "Lane", speciality: "Extensions" , salon: " Cetana Salon Spa",
+       salon_address: "155 Brannan St San Francisco, CA 94107", 
+       headshot: "stylist3.jpg" , email: "mee3@me.com",
+       phone: "555-555-5555", school: "Paul Mitchell", cut: "true", color: "true",
+      updo: "false" , natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
 
 
-Stylist.create({first_name: "Vien", last_name: "Van", 
-	school: "General Assembly", salon: "We Heart Hair",
-	email: "me4@me.com", headshot: "bday.jpg", password: "password"})
+
+    Stylist.create({first_name: "Robin", last_name: "Jordan", speciality: "Cutting", salon: " Adollup",
+       salon_address: "370 De Haro St San Francisco, CA 94103", 
+       headshot: "stylist4.jpg" , email: "mee4@me.com",
+       phone: "555-555-5555", school: "Toni & Guy", cut: "true", color: "false",
+      updo: "true", natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+     Stylist.create({first_name: "Prince", last_name: "Kim" , speciality: "Color"  , salon: " Salon Mio Mio",
+       salon_address: "1304 Castro San Francisco, CA 94114", 
+       headshot: "stylist5.jpg" , email: "mee5@me.com",
+       phone: "555-555-5555", school: "Paul Mitchell" , cut: "true", color: "true",
+      updo: "false" , natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+ Stylist.create({first_name: "Jada" , last_name: "Riss" , speciality: "Updos" , salon: "The Cutlery",
+       salon_address: "1239A 9th Ave San Francisco, CA 94122" , 
+       headshot: "stylist6.jpg" , email: "mee6@me.com",
+       phone: "555-555-5555", school: "Toni & Guy" , cut: "true", color: "false" ,
+      updo: "true", natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+ Stylist.create({first_name: "Carolyn", last_name: "Fecteau" , speciality: "Color"  , salon: "TAYLOR / MONROE",
+       salon_address: "448 Grove St San Francisco, CA 94102", 
+       headshot: "stylist7.jpg", email: "mee7@me.com",
+       phone: "555-555-5555", school: "Toni & Guy", cut: "false" , color: "true",
+      updo: "false", natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+
+ Stylist.create({first_name: "Leslie", last_name: "Ovit", speciality: "Extensions" , salon:" Honeycomb Salon",
+       salon_address: "3915 24th St San Francisco, CA 94114", headshot: "stylist8.jpg" , email: "mee8@me.com",
+       phone: "555-555-5555", school: "Aveda Institute", cut: "false" , color: "true",
+      updo: "false", natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+  Stylist.create({first_name: "Sara", last_name: "Simpler", speciality: "Color", 
+  	salon: "Bryan Roberts Salon & Color Bar",
+       salon_address: "561 Castro St San Francisco, CA 94114", headshot: "stylist9.jpg" , email: "mee9@me.com",
+       phone: "555-555-5555", school: "Aveda Institute", cut: "true", color: "true" ,
+      updo: "true", natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+Stylist.create({first_name: "Victoria", last_name: "Olen", speciality: "Extensions" , salon: "Population Salon",
+       salon_address: "1041 Guerrero San Francisco, CA 94110", headshot: "stylist10.jpg" , email: "mee10@me.com",
+       phone: "555-555-5555", school: "Aveda Institute", cut: "false" , color: "true",
+      updo: "false", natural: "false", bio: "Lorem ipsum dolor sit amet, 
+      consectetur adipisicing elit. Fuga, eum. Fuga 
+      laboriosam repellat, 
+      nemo itaque aspernatur quas cumque dignissimos illum 
+      atque libero, quisquam in dicta. Debitis qui, 
+      iste ut dolores.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing 
+elit. Quia 
+quis, aut consectetur cupiditate minus et voluptatibus 
+veniam, sit harum distinctio quos possimus quae? Temporibus 
+um eos, harum incidunt atque nulla.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Quidem ipsam commodi doloremque, recusandae tenetur. Cumque 
+ure labore numquam at, ipsum tempore ad voluptates nostrum 
+soluta exercitationem eos id architecto amet!" , password_digest: "password"})
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
