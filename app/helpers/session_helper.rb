fd11d5fbd839c3f98c88158fcff864ff62da0d48
Jasmine Martin <jasminejeane@gmail.com>
2016-02-24 21:03:48 -0800

    Logout switch works by adding @current_user to function

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -3,16 +3,15 @@ module SessionHelper
   def login(user)
     if user.class.name == 'Stylist'
       session[:user_id] = user.id
-      current_user = Stylist.find_by(id:user.id)
+      @current_user = Stylist.find_by_id(user.id)
     else
       session[:user_id] = user.id
-      current_user = Client.find_by(id:user.id)
+      @current_user = Client.find_by_id(user.id)
     end
   end
 
   def current_user
-    binding.pry
-    @current_user ||= Client.find_by(id:session[:user_id]) || Stylist.find_by(session[:user_id])
+    @current_user ||= Client.find_by_id(session[:user_id]) || Stylist.find_by_id(session[:user_id])
   end
 
   def logged_in?
@@ -22,9 +21,7 @@ module SessionHelper
   end
 
   def logout
-    binding.pry
     @current_user = session[:user_id] = nil
-    binding.pry
   end
 
 end
